1ef0fa408d1b335b3486551eccf93c649f0bc17f
Ankush <thakurankush947@gmail.com>
2019-01-25 00:09:54 +0530

    New String

--- app/src/main/java/com/doodhbhandaar/dbadmin/TaskBottomActivity.java
+++ app/src/main/java/com/doodhbhandaar/dbadmin/TaskBottomActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class TaskBottomActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private String a="jkbbdjk";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
