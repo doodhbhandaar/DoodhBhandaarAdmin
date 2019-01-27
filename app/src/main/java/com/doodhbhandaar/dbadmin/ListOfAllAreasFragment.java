e588bd3beb5149ff3f50039497555ea1fa5c1b28
shivamkrj <33348791+shivamkrj@users.noreply.github.com>
2019-01-27 21:05:00 +0530

    checkpoint 6

---
+++ app/src/main/java/com/doodhbhandaar/dbadmin/ListOfAllAreasFragment.java
@@ -0,0 +1,124 @@
+package com.doodhbhandaar.dbadmin;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListOfAllAreasFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    ArrayList<String> areaItems;
+    AreasAdapter adapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference areasReference;
+
+    public ListOfAllAreasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_of_all_areas, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fabAreasAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                addArea();
+            }
+        });
+        recyclerView = view.findViewById(R.id.areas_recycler_view);
+        areaItems = new ArrayList<>();
+
+        FirebaseApp.initializeApp(getContext());
+        firebaseDatabase = FirebaseDatabaseReference.getDatabaseInstance();
+        areasReference = firebaseDatabase.getReference("AREAS");
+        areasReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot dsp: dataSnapshot.getChildren()){
+                    String s = String.valueOf(dsp.getValue());
+                    if(s!=null)
+                        areaItems.add(s);
+//                    Toast.makeText(getContext(),s+ " +",Toast.LENGTH_SHORT).show();
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        adapter = new AreasAdapter(getContext(),areaItems);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        return view;
+    }
+
+    private void addArea() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View dialogView = getLayoutInflater().inflate(R.layout.add_area_dialog_layout,null);
+        builder.setView(dialogView);
+        final EditText areaEditText = dialogView.findViewById(R.id.area_add_edit_text);
+
+        builder.setTitle("Add Area");
+        builder.setCancelable(true);
+        builder.setMessage("Enter Area");
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                //Toast.makeText(MainActivity.this,"Ok Presses",Toast.LENGTH_LONG).show();
+                String newArea = "Panchawati ";
+                newArea = areaEditText.getText().toString();
+                areasReference.push().setValue(newArea);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //TODO
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
